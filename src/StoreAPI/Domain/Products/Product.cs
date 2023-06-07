057c35cf48279597dbd5f889e1cd0b59c8ab52fd
luelisan98 <38588780+lesan2807@users.noreply.github.com>
2023-06-06 19:43:18 -0600

    Add data validation for Product

--- src/StoreAPI/Domain/Products/Product.cs
+++ src/StoreAPI/Domain/Products/Product.cs
@@ -4,21 +4,28 @@
 public class Product
 {
     [FirestoreProperty]
+	[Required(ErrorMessage = "Name is required")]
     public string Name { get; set; }
 
     [FirestoreProperty]
+	[Range(0, double.MaxValue, ErrorMessage = "Price must be a positive value")]
     public double Price { get; set; }
 
     [FirestoreProperty]
+	[Range(0, double.MaxValue, ErrorMessage = "Stock must be a positive value")]
     public double Stock { get; set; }
 
     [FirestoreProperty]
+	[Required(ErrorMessage = "Image URL is required")]
+    [Url(ErrorMessage = "Image must be a valid URL")]
     public string Image { get; set; }
 
     [FirestoreProperty]
+	[Required(ErrorMessage = "Description is required")]
     public string Description { get; set; }
 
     [FirestoreProperty]
+	[Required(ErrorMessage = "Category is required")]
     public string Category { get; set; }
 
 
