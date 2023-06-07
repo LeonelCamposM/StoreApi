331cff85304367fe78ba43b61624c397ddd9366c
luelisan98 <38588780+lesan2807@users.noreply.github.com>
2023-06-06 20:32:43 -0600

    Add error handling 200, 404 and 500

--- src/StoreAPI/Controllers/ProductController.cs
+++ src/StoreAPI/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using static Google.Cloud.Firestore.V1.StructuredQuery.Types;
+using System.Net;
 
 namespace StoreAPI.Controllers
 
@@ -19,14 +20,25 @@ namespace StoreAPI.Controllers
         }
 
         [HttpGet]
-        public async Task<IEnumerable<Product>> Get()
+        public async Task<IActionResult> Get()
         {
             var eventId = new EventId(0001, "RequestedProducts");
-            IEnumerable < Product > products =  await _productService.GetAllAsync();
-            _logger.LogInformation(eventId, "products requested  at: {time} :  ", DateTimeOffset.UtcNow);
-            _logger.LogWarning("This is a warning message.");
-            _logger.LogError("This is a error message.");
-            return  products;
+			try
+			{
+				IEnumerable<Product> products = await _productService.GetAllAsync();
+				_logger.LogInformation(eventId, "Products requested at: {time}", DateTimeOffset.UtcNow);
+				if (products == null || !products.Any())
+				{
+					_logger.LogInformation(eventId, "No products found at: {time}", DateTimeOffset.UtcNow);
+					return StatusCode((int)HttpStatusCode.NotFound, "No products found"); // HTTP 404 Not Found status
+				}
+				return Ok(products); // HTTP 200 OK status
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(eventId, ex, "An error occurred while retrieving products");
+				return StatusCode((int)HttpStatusCode.InternalServerError, "An error occurred"); // HTTP 500 Internal Server Error status
+			}
         }
     }
 }
