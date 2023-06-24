2b59d99727461a8317b2f7dd11c5c83787620189
LeonelCamposM <leonelscamposm@gmail.com>
2023-06-24 10:37:07 -0600

    add UserFactory

---
+++ src/StorePresentation/Infrastructure/AzureADB2CUserFactory.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication.Internal;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace StorePresentation.Infrastructure
+{
+    public class AzureADB2CUserFactory : AccountClaimsPrincipalFactory<RemoteUserAccount>
+    {
+
+        public AzureADB2CUserFactory(IAccessTokenProviderAccessor accessor) : base(accessor)
+        {
+        }
+
+        public async override ValueTask<ClaimsPrincipal> CreateUserAsync(RemoteUserAccount account, RemoteAuthenticationUserOptions options){
+            var initialUser = await base.CreateUserAsync(account, options);
+            var userIdentity = initialUser.Identity as ClaimsIdentity;
+            if (initialUser.Identity.IsAuthenticated)
+            {
+                string token_str = "";
+                string role = "";
+                var tokenResult = await TokenProvider.RequestAccessToken();
+                if (tokenResult.TryGetToken(out var token))
+                {
+                    token_str = token.Value;
+                    role = GetUserRole(token_str);
+                }
+                userIdentity.AddClaim(new Claim(ClaimTypes.Role, role));
+            }
+            return initialUser;
+        }
+
+        private string GetUserRole(string token_str)
+        {
+            var role = "";
+            if (token_str != "")
+            {
+                var handler = new JwtSecurityTokenHandler();
+                var token = handler.ReadJwtToken(token_str);
+                var userName = token.Claims.FirstOrDefault(c => c.Type == "given_name")?.Value;
+                role = token.Claims.FirstOrDefault(c => c.Type == "extension_role")?.Value;
+            }
+            return role;
+        }
+    }
+}
--- src/StorePresentation/Program.cs
+++ src/StorePresentation/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using StorePresentation;
+using StorePresentation.Infrastructure;
+using System.Security.Claims;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,6 +14,7 @@ builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
     options.ProviderOptions.DefaultAccessTokenScopes.Add("https://StoreApicr.onmicrosoft.com/cb131417-eaa3-430b-8643-f46e44bf1b1c/Api.ReadWrite");
-});
+    options.UserOptions.RoleClaim = ClaimTypes.Role;
+}).AddAccountClaimsPrincipalFactory<AzureADB2CUserFactory>();
 
 await builder.Build().RunAsync();
