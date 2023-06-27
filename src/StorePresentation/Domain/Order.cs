e42098246561146296d4d875405b483066fc4d00
LeonelCamposM <leonelscamposm@gmail.com>
2023-06-26 20:20:27 -0600

    refactor code

---
+++ src/StorePresentation/Domain/Order.cs
@@ -0,0 +1,18 @@
+﻿namespace StorePresentation.Domain
+{
+    public class Order
+    {
+        public string Address { get; set; }
+        public double Total { get; set; }
+        public DateTime Date { get; set; }
+        public string Email { get; set; }
+
+        public Order(string address, double total, DateTime date, string email)
+        {
+            Address = address;
+            Total = total;
+            Date = date;
+            Email = email;
+        }
+    }
+}
