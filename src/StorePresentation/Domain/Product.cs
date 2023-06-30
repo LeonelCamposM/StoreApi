aa6fadf443027b166c9a793ed7bce667ea6c5671
luelisan98 <38588780+lesan2807@users.noreply.github.com>
2023-06-29 20:29:19 -0600

    Api calls for add and edit product with the UI

--- src/StorePresentation/Domain/Product.cs
+++ src/StorePresentation/Domain/Product.cs
@@ -1,13 +1,28 @@
-﻿namespace StorePresentation.Domain
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StorePresentation.Domain
 {
     public class Product
     {
         public string Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Price must be a positive value")]
         public double Price { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Stock must be a positive value")]
         public double Stock { get; set; }
+
+        [Required(ErrorMessage = "Image URL is required")]
+        [Url(ErrorMessage = "Image must be a valid URL")]
         public string Image { get; set; }
+        
+        [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "Category is required")]
         public string Category { get; set; }
 
         public Product(string id, string name, double price, double stock, string image, string description, string category)
